e15b6fdbe957e1d7282cb8b46743c35404c98328
pcdjohnson <pcdjohnson@gmail.com>
2024-10-03 12:41:39 +0100

    Minor edits

--- PowerAnalysis.R
+++ PowerAnalysis.R
@@ -201,7 +201,7 @@ cat("# SchistoDrivers\n\n",
     file = readme.file)
 
 
-### Appendix: Additional power analyses ### 
+### Appendix: Additional power analysis ### 
 
 if(FALSE) {
   
@@ -217,60 +217,21 @@ if(FALSE) {
   #    AUC is normally distributed within each group (it is)
   #    We know the SD of the AUC within each group (it's 403 units)
   #    We know what our target effect size is, i.e. the smallest effect that we want to detect
-  #    alpha = 0.05 and target power = 80%.  
+  #    alpha = 0.05 and target power = 90%.  
   
   delta.AUC <- seq(0.05, 1, 0.05)
+  target.power <- 0.9
   N.per.group <- 
     ceiling(sapply(delta.AUC, function(delta) {
-      power.anova.test(groups = 3, power = 0.9, between.var = var(c(0, delta, delta * 2)), 
+      power.anova.test(groups = 3, power = target.power, between.var = var(c(0, delta, delta * 2)), 
                        within.var = 1)$n
     }))
   plot(delta.AUC, N.per.group, log = "y", type = "b")
-  title("Sample size required per group for 80% power at alpha = 0.05\ngiven target delta.AUC in SD units")
+  title.text <-
+    paste0("Sample size required per group for ", target.power * 100, 
+           "% power at alpha = 0.05\ngiven target delta.AUC in SD units")
+  title(title.text)
   cbind(delta.AUC, N.per.group)
   
-  
-  # Additional power analysis for comparison of eggs per gram between the three groups:
-  epg.effect <- 3
-  # Based on existing data mean and SD of EPG in fed subjects following treatment
-  scaling.factor <- 24 # because EPG is calculated from >1 gram of faeces
-  lambda <- 0.2128099 * scaling.factor
-  s <- 2.45669 * scaling.factor
-  theta <- lambda^2 / (s^2 - lambda) # calculate dispersion parameter from SD and mean
-  # check we get the right mean and SD
-  mean(rnbinom(100000, size = theta, mu = lambda))
-  sd(rnbinom(100000, size = theta, mu = lambda))
-
-    
-  # estimate power
-  power.estimate <- 
-    unlist(mclapply(N.per.group, function(N) {
-      # N <- 17
-      p.value <-
-        replicate(n.sim, {
-          dat <- expand.grid(id = 1:N, group = c("home", "brought", "provided"))
-          dat$lambda <- lambda * (4 - as.numeric(dat$group))
-          dat$epg <- rnbinom(nrow(dat), size = theta, mu = dat$lambda)
-          (tab <- table(dat$epg, dat$group))
-          # if the data is unanalysable (e.g. all zeros) then p.val=1 (effectively)
-          if(any(tapply(dat$epg, dat$group, function(x) length(unique(x))) == 1)) return(1) 
-          
-          #fit <- glm.nb(epg ~ group, data = dat)
-          #coef(summary(fit))["groupunfed", "Pr(>|z|)"]
-          # ...switch from glm.nb to glmmTMB as it converges better at low N
-          fit <- glmmTMB(epg ~ group, data = dat, family = nbinom2)
-          fit0 <- update(fit, ~ 1)
-          p.val <- anova(fit0, fit)[2, "Pr(>Chisq)"]
-          if(is.nan(p.val)) return(1) else return(p.val)
-        })
-      mean(p.value < 0.05)
-    }, mc.cores = detectCores()))
-  
-  plot(N.per.group, power.estimate, type = "b", log = "x", ylim = 0:1)
-  abline(h = 0.8, lty = 3)
-  title(paste0("Power to detect an unfed:fed ratio in EPG of ", 
-               epg.effect, "x\nassuming mean (SD) fed EPG = ", 
-               round(lambda, 2), " (", round(s, 2), ")\nestimated from ", n.sim, " simulated data sets"))
-  cbind(N.per.group, power.estimate)
 
 }
